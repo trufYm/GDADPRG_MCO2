cc20fe091c8c81f090eb64af9a6039443ddfc29c
WaywardTaco <aviso.josh@gmail.com>
2024-02-01 08:24:39 +0800

    include

---
+++ Inclusions.hpp
@@ -0,0 +1,10 @@
+
+#pragma once
+
+#include <iostream>
+#include <unordered_map>
+#include "SFML/Graphics.hpp"
+
+#include "Controller/Game.cpp"
+#include "Controller/Manager/TextureManager.cpp"
+#include "Model/GameObject.cpp" //Might be removed? Specs specify a GameObjectManager, maybe GameObject inherits from the manager?
--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,5 @@
-#include <iostream>
-#include <unordered_map>
-#include "SFML/Graphics.hpp"
 
-#include "Controller/Game.cpp"
-#include "Controller/Manager/TextureManager.cpp"
-#include "Model/GameObject.cpp" //Might be removed? Specs specify a GameObjectManager, maybe GameObject inherits from the manager?
+#include "Inclusions.hpp"
 
 int main() {
    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML Start-Up");
